02c901ee419eeb27ea5bbdfd62a93de8b7125265
Hanne Soubry <hanne.soubry@student.vives.be>
2025-04-01 17:12:15 +0200

    Update labo 8 contact manager
    - uitbreiding contacten sorteren en verwijderen

--- Labo 08/ContactManager/scripts/code.js
+++ Labo 08/ContactManager/scripts/code.js
@@ -1,5 +1,6 @@
 let global = {
     personen: [],
+    nextId: 0,
     currentFormId: -1
 };
 
@@ -7,68 +8,63 @@ let global = {
 const bewaarBewerktePersoon = () => {
     if(valideer()) {
         let lijst = document.getElementById("lstPersonen");
+        let index = global.personen.findIndex((element) => element.id === global.currentFormId);
 
         if(global.currentFormId === -1) {
             // een nieuw aangemaakte persoon voegen we toe
-            global.currentFormId = global.personen.length;
-            global.personen.push(leesPersoon());
-
-            // toevoegen aan lijst
-            lijst.appendChild(document.createElement("option"));
-            lijst.selectedIndex = global.currentFormId;
+            global.currentFormId = global.nextId;
+            global.nextId++;
+            global.personen.push(formNaarPersoon());
         }
         else {
             // een bestaande persoon in de lijst overschrijven
-            global.personen[global.currentFormId] = leesPersoon();
+            global.personen[index] = formNaarPersoon();
         }
 
-        // lijst updaten
-        let option = lijst.children[global.currentFormId];
-        option.setAttribute("value", JSON.stringify(global.personen[global.currentFormId]));
-        option.textContent = global.personen[global.currentFormId].voornaam + " "
-            + global.personen[global.currentFormId].familienaam;
+        // sorteer lijst
+        global.personen.sort((a, b) => {
+            return (a.voornaam + " " + a.familienaam).toLowerCase().localeCompare((b.voornaam + " " + b.familienaam).toLowerCase())
+        });
+        lijst.innerHTML = "";   // remove all children
+        for(let i = 0; i < global.personen.length; i++) {
+            let option = lijst.appendChild(document.createElement("option"));
+            option.value = global.personen[i].id;
+            option.textContent = global.personen[i].voornaam + " " + global.personen[i].familienaam;
+        }
+
+        index = global.personen.findIndex((element) => element.id === global.currentFormId);
+        lijst.selectedIndex = index;
     }
 };
 
-const leesPersoon = () => {
+const formNaarPersoon = () => {
+    // read from form
     let voornaam = document.getElementById("txtVoornaam");
     let familienaam = document.getElementById("txtFamilienaam");
     let geboorteDatum = document.getElementById("txtGeboorteDatum");
     let email = document.getElementById("txtEmail");
     let kinderen = document.getElementById("txtAantalKinderen");
 
+    // write into person object
     return {
         voornaam: voornaam.value,
         familienaam: familienaam.value,
         geboorteDatum: new Date(geboorteDatum.value),
         email: email.value,
-        kinderen: parseInt(kinderen.value, 10)
+        kinderen: parseInt(kinderen.value, 10),
+        id: global.currentFormId
     };
 }
 
-const schrijfPersoon = () => {
+const nieuwePersoon = () => {
+    // get form fields
     let voornaam = document.getElementById("txtVoornaam");
     let familienaam = document.getElementById("txtFamilienaam");
     let geboorteDatum = document.getElementById("txtGeboorteDatum");
     let email = document.getElementById("txtEmail");
     let kinderen = document.getElementById("txtAantalKinderen");
 
-    let persoon = global.personen[global.currentFormId];
-
-    voornaam.value = persoon.voornaam;
-    familienaam.value = persoon.familienaam;
-    geboorteDatum.value = persoon.geboorteDatum.toLocaleDateString();
-    email.value = persoon.email;
-    kinderen.value = persoon.kinderen;
-}
-
-const bewerkNieuwePersoon = () => {
-    let voornaam = document.getElementById("txtVoornaam");
-    let familienaam = document.getElementById("txtFamilienaam");
-    let geboorteDatum = document.getElementById("txtGeboorteDatum");
-    let email = document.getElementById("txtEmail");
-    let kinderen = document.getElementById("txtAantalKinderen");
-
+    // reset fields
     voornaam.value = "";
     familienaam.value = "";
     geboorteDatum.value = "";
@@ -77,16 +73,52 @@ const bewerkNieuwePersoon = () => {
 
     global.currentFormId = -1;
 
+    // deselect
     let lijst = document.getElementById("lstPersonen");
     lijst.selectedIndex = -1;
 };
 
 const checkSelection = () => {
+    // read selected id
     let lijst = document.getElementById("lstPersonen");
-    global.currentFormId = lijst.selectedIndex;
-    schrijfPersoon();
+    let id = parseInt(lijst.options[lijst.selectedIndex].value, 10);
+    global.currentFormId = id;
+
+    // find person with this id
+    let index = global.personen.findIndex((element) => element.id === id);
+    let persoon = global.personen[index];
+
+    // get form fields
+    let voornaam = document.getElementById("txtVoornaam");
+    let familienaam = document.getElementById("txtFamilienaam");
+    let geboorteDatum = document.getElementById("txtGeboorteDatum");
+    let email = document.getElementById("txtEmail");
+    let kinderen = document.getElementById("txtAantalKinderen");
+
+    // update form fields
+    voornaam.value = persoon.voornaam;
+    familienaam.value = persoon.familienaam;
+    geboorteDatum.value = persoon.geboorteDatum.toLocaleDateString();
+    email.value = persoon.email;
+    kinderen.value = persoon.kinderen;
 }
 
+const verwijder = () => {
+    if(global.currentFormId !== -1) {
+        // if existing person is selected
+        // remove data
+        let index = global.personen.findIndex((element) => element.id === global.currentFormId);
+        global.personen.splice(index);
+
+        // remove from list
+        let lijst = document.getElementById("lstPersonen");
+        let option = lijst.children[index];
+        lijst.removeChild(option);
+
+        // reset form
+        nieuwePersoon();
+    }
+}
 
 // onze setup functie die de event listeners registreert
 const setup = () => {
@@ -94,10 +126,13 @@ const setup = () => {
     btnBewaar.addEventListener("click", bewaarBewerktePersoon);
 
     let btnNieuw = document.getElementById("btnNieuw");
-    btnNieuw.addEventListener("click", bewerkNieuwePersoon);
+    btnNieuw.addEventListener("click", nieuwePersoon);
 
     let lstPersonen = document.getElementById("lstPersonen");
     lstPersonen.addEventListener("click", checkSelection);
+
+    let btnVerwijder = document.getElementById("btnVerwijder");
+    btnVerwijder.addEventListener("click", verwijder);
 };
 
 window.addEventListener("load", setup);
